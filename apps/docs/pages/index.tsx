968c3ddcb22fe4d8fde6b41aef179c8661cb2ce4
lucas.contini@consulting-for.edenred.com <lucaschezin@gmail.com>
2023-01-09 16:06:22 -0300

    initial

--- apps/docs/pages/index.tsx
+++ apps/docs/pages/index.tsx
@@ -1,10 +1,10 @@
-import { Button } from "ui";
+import { Button } from "../../../packages/ui/Button";
 
 export default function Docs() {
   return (
     <div>
       <h1>Docs</h1>
-      <Button />
+      <Button></Button>
     </div>
   );
 }
--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,7 +1,25 @@
-module.exports = {
-  extends: ["next", "turbo", "prettier"],
-  rules: {
-    "@next/next/no-html-link-for-pages": "off",
-    "react/jsx-key": "off",
-  },
+const validatePath = (path) => {
+  const isPathCorrect = path.length > 0;
+  return  isPathCorrect ? path : 'deu pau';
 };
+
+
+const eslintrc = (path) => {
+  return (
+    "extends": ['next', 'turbo', 'prettier', 'airbnb'],
+    "rules": {
+      '@next/next/no-html-link-for-pages': 'off',
+      'react/jsx-key': 'off',
+      'react/jsx-uses-react': 'off',
+      'react/react-in-jsx-scope': 'off',
+      'import/prefer-default-export': 'warn',
+      'import/jsx-props-no-spreading': 'off',
+    },
+    parserOptions: {
+      project: '../../\'\$\{path\}\'/tsconfig/base.json',
+    },
+  }
+  )
+};
+
+module.exports = eslintrc;
