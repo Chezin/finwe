b53609cfdeb20b53d2736d692de1ee35d7214650
MaaTReD <matheus0913@gmail.com>
2023-02-07 17:55:48 -0300

    Update nav bar logo

--- apps/web/components/NavBar.tsx
+++ apps/web/components/NavBar.tsx
@@ -3,8 +3,8 @@ import * as bundle from '../bundles/pt_BR'
 export default function NavBar() {
   return (
     <div className="navbar bg-purple-900 pr-16 pl-16 h-24">
+      <img alt="Inicius" src="https://admin.refugioacai.com/btti-static/btti-logo-main.png"/>
       <button type="button" className="navbar-start font-bold text-xl">
-        {bundle.brandName}
       </button>
       <div className="navbar-center">
         <button type="button" className="btn btn-ghost">CACILDIS</button>
